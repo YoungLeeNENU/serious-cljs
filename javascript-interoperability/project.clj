d0b9c0a4e48a351f4dd2274ad2edd654dfe2e919
YoungLeeNENU <youngleemails@gmail.com>
2016-07-24 00:43:58 +0800

    刚才magit抽风，可算能commit了。。。

--- javascript-interoperability/project.clj
+++ javascript-interoperability/project.clj
@@ -1,6 +1,25 @@
-(defproject javascript-interoperability "0.1.0-SNAPSHOT"
+(defproject figwheel-project "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+
   :url "http://example.com/FIXME"
+  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.51"]]
+
+  :plugins [[lein-cljsbuild "1.1.0"]
+            [lein-figwheel "0.5.1"]]
+
+  :clean-targets [:target-path "out"]
+
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :figwheel true
+              :compiler {:main "javascript-interoperability.core"
+                         :asset-path "js/out"
+                         :output-to "resources/public/js/main.js"
+                         :output-dir "resources/public/js/out"}}]})
+
--- javascript-interoperability/src/javascript_interoperability/core.cljs
+++ javascript-interoperability/src/javascript_interoperability/core.cljs
@@ -1,3 +1,40 @@
 (ns javascript-interoperability.core)
 
+(enable-console-print!)
 
+
+(let [js-var-a (array 1 2 3)
+      cljs-symbol-b [1 2 3]]
+
+  (defn show-dom-msg [judge]
+    "Show Message on Page"
+    (if (= judge true)
+      (set! (.-innerHTML (js/document.getElementById "msg-container")) ;js object 里属性的使用
+            "Same Data")
+      (set! (.-innerHTML (js/document.getElementById "msg-container"))
+            "Different Data")))
+  (do
+    ;; 和 js 变量互相转换
+    ;; (.log js/console (type js-var-a))
+    (println (type js-var-a))
+    ;; (.log js/console (type cljs-symbol-b))
+    (println (type cljs-symbol-b))
+    ;; (.log js/console (type (js->clj js-var-a)))
+    (println (type (js->clj js-var-a)))
+    ;; (.log js/console (type (clj->js cljs-symbol-b)))
+    (println (type (clj->js cljs-symbol-b)))
+
+    ;; 不可变数据
+    #_(if (= js-var-a (array 1 2 3))
+        (show-dom-msg true)
+        (show-dom-msg false))
+    #_(if (= cljs-symbol-b [1 2 3])
+        (show-dom-msg true)
+        (show-dom-msg false))
+    #_(if (= (js->clj js-var-a) [1 2 3])
+        (show-dom-msg true)
+        (show-dom-msg false))
+    #_(if (= (clj->js cljs-symbol-b) (array 1 2 3))
+        (show-dom-msg true)
+        (show-dom-msg false))
+    ))
