bf47347d23e0bcab6600f6e1eff0e9f02d410900
YoungLeeNENU <youngleemails@gmail.com>
2016-07-23 09:59:08 +0800

    update

---
+++ dom-operations/src/dom_operations/core.clj
@@ -0,0 +1,6 @@
+(ns dom-operations.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ dom-operations/test/dom_operations/core_test.clj
@@ -0,0 +1,7 @@
+(ns dom-operations.core-test
+  (:require [clojure.test :refer :all]
+            [dom-operations.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
